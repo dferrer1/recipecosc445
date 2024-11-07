ec7c8db56493f46d058543d160b2135933a5e303
jmuncy2 <71195274+jmuncy2@users.noreply.github.com>
2024-11-07 21:03:52 +0000

    simple frontend added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,34 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+st.sidebar.title("Menu")
+menu = st.sidebar.radio("Select a page:", ["Home", "Results", "About"])
+
+if menu == "Home":
+    file_path = 'frontend/home.txt'
+
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    st.markdown(content)    
+
+
+if menu == "Results":
+    st.title("Results")
+    st.write(
+        "This page is dedicated to showcasing our results and charts."
 )
+
+   
+elif menu == "About":
+    
+    file_path = 'frontend/about.txt'
+
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    st.markdown(content)
+    
+
