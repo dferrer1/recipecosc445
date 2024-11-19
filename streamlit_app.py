3f192e3e6da78498b7f79f89c0b0080b10e4d33b
Diego Ferrer <dferrer1@vols.utk.edu>
2024-11-19 10:00:33 -0500

    Add common ingredients analysis

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,13 @@ if menu == "Results":
     st.title("Results")
     st.write(
         "This page is dedicated to showcasing our results and charts."
-)
+    )
+    st.image('images/Top 15 Most Commonly Used Ingredients.png')
+    st.image('images/Top 30 Recipes Using xyz.png')
+    st.image('images/Top 30 Recipes Excluding xyz.png')
+    st.image('images/Top 10 Recipes Using x.png')
+    st.image('images/Top 10 Recipes Using xy.png')
+    st.image('images/Top 10 Recipes Using xyz.png')
 
    
 elif menu == "About":
